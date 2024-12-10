59aeaba0f3743b71d0deff5b9eea4e2b3a8f7652
mahdihaghollahi <mahdiaghollahi@gmail.com>
2024-12-10 10:46:50 +0330

    add tehran map

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "tehran map",
-  description: "tehran map street",
+  description: "tehran map",
 };
 
 export default function RootLayout({
